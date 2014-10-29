66949173306ac24f631f5df1b9fd0fc7a74a40dd
felica raspberryPI <naoya@cps.im.dendai.ac.jp>
2014-10-29 21:19:20 +0000

    ほぼ完成

--- testLight.py
+++ testLight.py
@@ -1,25 +1,24 @@
+# -*- coding: utf-8 -*-
 import RPi.GPIO as GPIO
 import time
+import os
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(21,GPIO.OUT)
-GPIO.output(21,True)
 
-def trueout():
-	GPIO.setup(18,GPIO.OUT)
-	GPIO.output(18,True)
+GPIO.setup(20,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(16,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def falseout():
-	GPIO.setup(18,GPIO.OUT)
-	GPIO.output(18,False)
-	
-def inputButton():
-	GPIO.setup(18,GPIO.IN)
-	GPIO.setup(15,GPIO.IN)
-	while True:
-		print '18 to'
-		print GPIO.input(18)
-		print '15 to'
-		print GPIO.input(15)
-		time.sleep(0.2)
+enterPath = u'./enter.sh'
+exitPath = u'./exit.sh'
+
+while True:
+	if GPIO.input(16) == False:
+		GPIO.output(21,True)
+		os.system(enterPath)
+		GPIO.output(21,False)
+	elif GPIO.input(20) == False:
+		GPIO.output(21,True)
+		os.system(exitPath)
+		GPIO.output(21,False)
