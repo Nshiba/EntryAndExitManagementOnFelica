14c9542b37811cd0a5c16688458f92c9f021a04c
felica raspberryPI <naoya@cps.im.dendai.ac.jp>
2014-11-01 22:24:45 +0000

    ツイートok

--- mytwitter.py
+++ mytwitter.py
@@ -3,10 +3,10 @@
 import requests as req
 from requests_oauthlib import OAuth1
 
-api_key = "v9nCQxM4HxqQNIBzNfPH4Ex2V" 
-api_secret = "QLifWZUrlmzaqKkdwOeZlTgYkqD3DfVdJZ7c10erKvTMn24sWn"  
-token = "2206303868-nQaHYODrMPmra1RPNaiZv35N9TTDKLSMUxqipUP" 
-token_secret = "PjnL1mKP8DXhAnCC9VO6TIYwtrnUkDx7lMTSHSt7BX4fH"
+api_key = "" 
+api_secret = ""  
+token = "" 
+token_secret = ""
 
 auth = OAuth1(api_key, api_secret, token, token_secret)
 
