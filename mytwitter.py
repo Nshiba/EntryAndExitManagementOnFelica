5699757de027e9443453d058e9e07d4cfc85e514
naoya <naoya.shiba310@gmail.com>
2014-11-02 07:29:33 +0900

    key削除

--- mytwitter.py
+++ mytwitter.py
@@ -3,10 +3,10 @@
 import requests as req
 from requests_oauthlib import OAuth1
 
-api_key = 'VVeTWB7z2tnOjSGRqNmJQVEPE'
-api_secret = 'VKjmZloi2RYam7ioeN4O5Wa67DZEp2zLbUm6ykrxbnor04xrrE'
-token = '2819411641-IdNCtvx2gY5AxRidHVhPUhck330EDu0iE97nZow'
-token_secret = 'LmgFcdXcrIaTGL3NVpiDOSxOGrW5puxnkwyde0LfWIN8B'
+api_key = ''
+api_secret = ''
+token = ''
+token_secret = ''
 
 auth = OAuth1(api_key, api_secret, token, token_secret)
 
